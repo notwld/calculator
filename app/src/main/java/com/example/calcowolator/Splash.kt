4416d35ca5865765b79ede2d1acd02291cbdd648
notwld <mwfarrukh@gmail.com>
2023-04-09 15:43:15 +0500

    layout

---
+++ app/src/main/java/com/example/calcowolator/Splash.kt
@@ -0,0 +1,19 @@
+package com.example.calcowolator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this,MainActivity::class.java))
+        },
+        3000
+            )
+    }
+}
